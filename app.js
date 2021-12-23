51721ae3a08abb66fa7b40dabefd9f4463d34305
Beyzanur <akinbeyzanur@gmail.com>
2021-12-24 01:20:30 +0300

    Edit Post Page

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ app.get('/', async (req, res) => {
   });
 });
 
+app.get('/post/:id', async (req,res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('post', {
+    post
+  });
+});
+
 app.get("/", (req, res) => {
   res.render('index');
 });
