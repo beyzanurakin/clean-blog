2584fc7f9ad3c334a7b09e2f1e52e52c5ded00c8
Beyzanur <akinbeyzanur@gmail.com>
2021-12-24 00:22:52 +0300

    Add mongoose model

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
+//npm i express mongoose express-fileupload ejs
+
+const Post = require('./models/Post');
+
 const app = express();
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=> {
+  console.log('DB CONNECTED!')
+}).catch((err)=> {
+  console.log(err)
+})
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 app.set("view engine", "ejs");
+
+app.post('/posts', async (req, res) => { // async - await yapısı kullanacğız.
+  await Post.create(req.body)            // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  res.redirect('/')
+});
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts
+  });
+});
 
 app.get("/", (req, res) => {
   res.render('index');
